9826ae007c808c6a3526cf11fc9a575a3a207a66
Manish <comm.manish@gmail.com>
2023-10-04 18:54:56 +0000

    learn end and sep, overwrite print default

--- hello.py
+++ hello.py
@@ -10,7 +10,12 @@ name = input("What's your name? ")
 #  Say hello to user
 
 # print("hello, " + name)
-print("hello,", name)
+# print("hello,", name)
+# print("hello,", end = " ")
+# print(name)
+print("hello,", name, sep = " what ")
+
+
 
 
 # arguments , "hello, world"(input) is a argument for print function

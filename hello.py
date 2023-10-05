aead3fd5d7b882e8fcab8f4140d538f62fd0d9f2
Manish <comm.manish@gmail.com>
2023-10-05 19:13:36 +0000

    strip, capitalize and title

--- hello.py
+++ hello.py
@@ -13,17 +13,35 @@ name = input("What's your name? ")
 # print("hello,", name)
 # print("hello,", end = " ")
 # print(name)
-print("hello,", name, sep = " what ")
+
+# print("hello,", name, sep = " what ")
+
 # positional parameters
+
 # named parameters
 # print("hello, "friend"")
 # print('hello, "friend"')
+
 # \ escape charater
-print("hello, \"friend\"")
+# print("hello, \"friend\"")
+
 # special string
+# print(f"hello, {name}")
+
+
+# Remove whitespace from str
+# name = name.strip()
+
+
+# Capitalize user's name
+
+# name = name.capitalize()
+# name = name.title()
+
+# Remove whitespace from str and capitalize user's name
+name = name.strip().title()
+
 print(f"hello, {name}")
-
-
 
 
 # arguments , "hello, world"(input) is a argument for print function
